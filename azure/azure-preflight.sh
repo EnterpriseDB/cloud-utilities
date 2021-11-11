92aa130042a39ab88852bc28e74e93d722ff9fd1
TQ <tianqiu.huang@enterprisedb.com>
2021-11-11 16:04:32 +0800

    fix: ignore zone restriction for d2_v4

--- azure/azure-preflight.sh
+++ azure/azure-preflight.sh
@@ -130,14 +130,9 @@ set -- "${POSITIONAL[@]}" # restore positional parameters
 [[ ! " ${AVAILABLE_PGTYPE[@]}" =~ "${pg_type}" ]] && show_help && echo "error: invalid PG instance type" && exit 1
 [[ ! " ${AVAILABLE_ENDPOINTS[@]}" =~ "${endpoint}" ]] && show_help && echo "error: invalid endpoint" && exit 1
 
-function infra_dsv2_vcpus()
-{
-    [ -z "$with_infra" ] && echo 0 || echo 6
-}
-
 function infra_dv4_vcpus()
 {
-    [ -z "$with_infra" ] && echo 0 || echo 6
+    [ -z "$with_infra" ] && echo 0 || echo 8
 }
 
 function infra_esv3_vcpus()
@@ -345,8 +340,8 @@ function sku_suggest()
 {
     local restriction=$1
     local sku=$2
-    if [ "$restriction" = "None" ]; then
-        suggest "$restriction" ok
+    if [ "$restriction" = "None" ] || ([[ $restriction == *"type: Zone"* ]] && [ "$sku" = "Standard_D2_v4" ]); then
+        suggest "None" ok
     else
         store_suggestion "virtualMachines SKU '$sku' has '$restriction'"
         suggest "$restriction" alert
@@ -368,10 +363,6 @@ printf "$FMT" "ResourceType" "Locations" "Name" "Zones" "Restrictions"
 printf "$FMT" "------------" "---------" "----" "-----" "------------"
 printf "$FMT" "virtualMachines" "$location" "Standard_D2_v4" "$(get_sku_zone_for Standard_D2_v4)" "$(sku_suggest "$sku_restriction_d2v4" "Standard_D2_v4")"
 printf "$FMT" "virtualMachines" "$location" "Standard_E2s_v3" "$(get_sku_zone_for Standard_E2s_v3)" "$(sku_suggest "$sku_restriction_e2sv3" "Standard_E2s_v3")"
-
-echo ""
-echo -e "\033[0;33mWarning: if the restriction type is 'Zone' for 'Standard_D2_v4', we could ignore it.\033[0m"
-echo ""
 
 #### Quota Limitation Checking
 function query_all_usage()
@@ -411,14 +402,12 @@ publicip_standard=($(get_nw_usage_for "Public IP Addresses - Standard"))
 
 # calculate available resources
 free_regional_vcpus=$((${regional_vcpus[1]} - ${regional_vcpus[0]}))
-free_dsv2_vcpus=$((${dsv2_vcpus[1]} - ${dsv2_vcpus[0]}))
 free_dv4_vcpus=$((${dv4_vcpus[1]} - ${dv4_vcpus[0]}))
 free_esv3_vcpus=$((${esv3_vcpus[1]} - ${esv3_vcpus[0]}))
 free_publicip_basic=$((${publicip_basic[1]} - ${publicip_basic[0]}))
 free_publicip_standard=$((${publicip_standard[1]} - ${publicip_standard[0]}))
 
 # calculate required resources
-need_dsv2_vcpus=$(infra_dsv2_vcpus)
 need_dv4_vcpus=$(infra_dv4_vcpus)
 need_esv3_vcpus=$(($(need_pg_vcpus_for $pg_type $ha)+$(infra_esv3_vcpus)))
 need_publicip_basic=$(need_public_ip)
@@ -426,7 +415,6 @@ need_publicip_standard=$(need_public_ip)
 
 # calculate gap of "need - free"
 gap_regional_vcpus=$((free_regional_vcpus - need_esv3_vcpus - need_dsv2_vcpus))
-gap_dsv2_vcpus=$((free_dsv2_vcpus - need_dsv2_vcpus))
 gap_dv4_vcpus=$((free_dv4_vcpus - need_dv4_vcpus))
 gap_esv3_vcpus=$((free_esv3_vcpus - need_esv3_vcpus))
 gap_publicip_basic=$((free_publicip_basic - need_publicip_basic))
@@ -449,7 +437,6 @@ FMT="%-32s %-8s %-8s %-11s %-8s %-11b\n"
 printf "$FMT" "Resource" "Limit" "Used" "Available" "Gap" "Suggestion"
 printf "$FMT" "--------" "-----" "----" "---------" "---" "----------"
 printf "$FMT" "Total Regional vCPUs" ${regional_vcpus[1]} ${regional_vcpus[0]} ${free_regional_vcpus} $gap_regional_vcpus "$(quota_suggest $gap_regional_vcpus "Total Regional vCPUs")"
-printf "$FMT" "Standard DSv2 Family vCPUs" ${dsv2_vcpus[1]} ${dsv2_vcpus[0]} ${free_dsv2_vcpus} $gap_dsv2_vcpus "$(quota_suggest $gap_dsv2_vcpus "Standard DSv2 Family vCPUs")"
 printf "$FMT" "Standard Dv4 Family vCPUs" ${dv4_vcpus[1]} ${dv4_vcpus[0]} ${free_dv4_vcpus} $gap_dv4_vcpus "$(quota_suggest $gap_dv4_vcpus "Standard Dv4 Family vCPUs")"
 printf "$FMT" "Standard ESv3 Family vCPUs" ${esv3_vcpus[1]} ${esv3_vcpus[0]} ${free_esv3_vcpus} $gap_esv3_vcpus "$(quota_suggest $gap_esv3_vcpus "Standard ESv3 Family vCPUs")"
 printf "$FMT" "Public IP Addresses - Basic" ${publicip_basic[1]} ${publicip_basic[0]} ${free_publicip_basic} $gap_publicip_basic "$(quota_suggest $gap_publicip_basic "Public IP Addresses - Basic")"
